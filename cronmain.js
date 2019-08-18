e3511143fc7cf983ae64982c6f5009f14a4b1273
aldoea <aldo.ea@hotmail.com>
2019-08-18 17:45:27 -0500

    Close of project

--- cronmain.js
+++ cronmain.js
@@ -1,21 +1,27 @@
 'use strict'
+var cron = require('node-cron');
 const {getSchedules} = require('./lib/scrapper.js');
 const {upsertAssignature} = require('./lib/queries.js');
 
-async function main(){
-    try {   
-        let results = await getSchedules()
-        console.log('Assignatures fetched:', results.length);
-        
-        for(const assignature of results) {
-            let dbres = await upsertAssignature(assignature);
-            //console.log(dbres.result);
-        }
-        process.exit(0);
+async function start(){
+    console.log('Cronmain loaded...');
+    
+    try { 
+        cron.schedule("* * * * *", async function() {
+            console.log('Cron start');
+            let results = await getSchedules()
+            console.log('Assignatures fetched:', results.length);
+            for(const assignature of results) {
+                let dbres = await upsertAssignature(assignature);
+            }
+            console.log('Cron ends');
+        });
     } catch (err) {
         console.log(err);
         process.exit(0);
     }
 }
-
-main();
+start();
+module.exports = {
+    start
+}
